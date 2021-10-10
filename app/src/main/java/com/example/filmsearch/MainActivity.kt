dce260a61cd1fd485072354762fa0cb8e39ab884
Tatyana Radyukhina <t.a.radyukhina@yandex.ru>
2021-10-10 17:25:33 +0300

    top_app_bar added

--- app/src/main/java/com/example/filmsearch/MainActivity.kt
+++ app/src/main/java/com/example/filmsearch/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.filmsearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ActionMode
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        topAppBar.setNavigationOnClickListener {
+            Toast.makeText(this, "Навигация", Toast.LENGTH_SHORT).show()
+
+        }
+
+        topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.fav -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.search -> {
+                    Toast.makeText(this, "Поиск", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.more -> {
+                    Toast.makeText(this, "Еще", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 }
+
+
+
