30e98f559e558437f6eda601a509e661e0f35f86
Tatyana Radyukhina <t.a.radyukhina@yandex.ru>
2021-10-19 14:04:39 +0300

    Add FrameLayout a1

--- app/src/main/java/com/example/filmsearch/MainActivity.kt
+++ app/src/main/java/com/example/filmsearch/MainActivity.kt
@@ -1,15 +1,13 @@
 package com.example.filmsearch
 
+import android.animation.*
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
-import android.view.ActionMode
 import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import androidx.annotation.Nullable
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.subscribe.view.*
@@ -20,8 +18,84 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button6.setOnClickListener {
+            val view = ImageView(this)
+            a1.addView(imageView6)
+        }
+
+        button6.setOnClickListener {
+            if(a1.childCount != 0) {
+                a1.removeViewAt(a1.childCount - 1)
+            }
+        }
+
+        a1.layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
+        a1.layoutTransition.setAnimator(LayoutTransition.APPEARING, AnimatorInflater.loadAnimator(this, R.animator.animator_file))
+
+
+
+        val anim = ObjectAnimator.ofFloat(imageView1, View.ALPHA, 0F, 1F)
+        anim.duration = 500
+
+        val anim1 = ObjectAnimator.ofFloat(imageView2, View.ALPHA, 0F, 1F)
+        anim.duration = 500
+
+        val anim2 = ObjectAnimator.ofFloat(imageView3, View.ALPHA, 0F, 1F)
+        anim.duration = 500
+
+        val anim3 = ObjectAnimator.ofFloat(imageView4, View.ALPHA, 0F, 1F)
+        anim.duration = 500
+
+        val animator = AnimatorSet()
+        animator.play(anim3).after(anim)
+        animator.start()
+
+        class SubscribeView(context: Context, @Nullable attributeSet: AttributeSet): LinearLayout(context, attributeSet){
+            private val editText: EditText
+            private val subscribeButton: Button
+
+            init{
+                LayoutInflater.from(context).inflate(R.layout.subscribe, this)
+                this.orientation = VERTICAL
+                editText = et_subscribe
+                subscribeButton = btn_subscribe
+                subscribeButton.setOnClickListener {
+                    Toast.makeText(context, editText.text, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
         button.setOnClickListener{
-            Toast.makeText(this,"Меню", Toast.LENGTH_SHORT).show()
+
+            val anim = ObjectAnimator.ofFloat(imageView1, View.ALPHA, 0F, 1F)
+            anim.duration = 500
+            anim.start()
+
+            val animationUpdateListener = object: Animator.AnimatorListener {
+                override fun onAnimationRepeat(p0: Animator?) {
+                    Toast.makeText(this@MainActivity, "Animation repeat", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onAnimationEnd(p0: Animator?) {
+                    Toast.makeText(this@MainActivity, "Animation End", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onAnimationCancel(p0: Animator?) {
+                    Toast.makeText(this@MainActivity, "Animation cancel", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onAnimationStart(p0: Animator?) {
+                    Toast.makeText(this@MainActivity, "Animation start", Toast.LENGTH_SHORT).show()
+                    println("start")
+                }
+            }
+
+            button.setOnClickListener {
+                val anim = ObjectAnimator.ofFloat(button, View.TRANSLATION_Y, 0F, -1000F)
+                anim.duration = 1000
+                anim.addListener(animationUpdateListener)
+                anim.start()
+            }
         }
 
         button2.setOnClickListener{
@@ -59,24 +133,12 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-
     }
 }
 
-class SubscribeView(context: Context, @Nullable attributeSet: AttributeSet): LinearLayout(context, attributeSet){
-    private val editText: EditText
-    private val subscribeButton: Button
 
-    init{
-        LayoutInflater.from(context).inflate(R.layout.subscribe, this)
-        this.orientation = VERTICAL
-        editText = et_subscribe
-        subscribeButton = btn_subscribe
-        subscribeButton.setOnClickListener {
-            Toast.makeText(context, editText.text, Toast.LENGTH_SHORT).show()
-        }
-    }
-}
+
+
 
 
 
